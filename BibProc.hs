6b7d968c1a725491828194c34a6d0081b1e44339
mr.mitch <mr.mitch@vfemail.net>
2010-10-30 23:54:23 +0200

    Fetch functions

--- BibProc.hs
+++ BibProc.hs
@@ -1,4 +1,10 @@
-data BibRecord = BibRecord CiteKey BibType [BibField]
+module BibProc where
+data BibRecord = BibRecord {
+  citeKey   :: CiteKey, 
+  bibType   :: BibType, 
+  bibFields :: [BibField]
+} deriving (Show)
+
 
 type CiteKey = String
 
@@ -12,19 +18,35 @@ data BibField = Authors [Personal]
                | Title String String 
                  deriving(Show)
 
-data Personal = Person PersonName 
+data Personal = Person {
+                       familyName :: String,
+                       givenName :: String
+                       } 
               | Institution String
                 deriving(Show)
                 
                 
 type PersonName = (String,String)
 
---data TitleInfo = Title String
---               | SubTitle String
---                 deriving(Show)
-                         
---showBibFields :: BibRecord -> String
---showBibFields [] = []
---showBibFields Authors:rest = "autor " ++ showBibFields rest
---showBibFields Titles:rest = "titulek " ++ showBibFields rest
---showBibFields _:rest = "nezname pole pole" ++ showBibFields rest
+
+sample = BibRecord "kkkOi" Article [
+  Title "Titulek" "Podtitulek", 
+  Authors [
+    Person "pepa" "novák",
+    Person "Jirka" "Kájínek"
+    ]
+  ]
+--ToDO: Pass test function
+fetchField :: (BibField -> Bool) -> [BibField] -> Maybe BibField
+fetchField _ [] = Nothing
+fetchField fn (x:xs) =  if (fn x) 
+                        then Just x
+                        else fetchField fn xs     
+
+testAuthor :: BibField -> Bool
+testAuthor (Authors _) = True
+testAuthor _ = False
+--fetchField (_:xs) = fetchField xs
+
+showBibFields :: BibRecord -> String
+showBibFields    (BibRecord k b fields) = "Záznam. Citekey: " ++ k 
