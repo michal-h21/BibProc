500ddf5fa441a36c9e89c383d352b1a6eb1c6efb
mr.mitch <mr.mitch@vfemail.net>
2010-10-31 21:32:01 +0100

    Some cleanup

--- BibProc.hs
+++ BibProc.hs
@@ -1,9 +1,13 @@
 module BibProc where
 data BibRecord = BibRecord {
   citeKey   :: CiteKey, 
-  bibType   :: BibType, 
+  bibTypes  :: [BibType], 
   bibFields :: [BibField]
-} deriving (Show)
+} deriving (Show, Eq,Read)
+
+class BibProc a where
+  bibImport :: a -> BibRecord
+  bibExport :: BibRecord -> a
 
 
 type CiteKey = String
@@ -13,39 +17,45 @@ data BibType = Article
              | InProceedings
              | WebPage 
              | Other String
-               deriving (Show)
+               deriving (Show,Eq,Read)
 data BibField = Authors [Personal]
                | Title String String 
-                 deriving(Show)
+                 deriving(Show,Eq,Read)
 
 data Personal = Person {
                        familyName :: String,
                        givenName :: String
                        } 
               | Institution String
-                deriving(Show)
+                deriving(Show,Eq,Read)
                 
                 
 type PersonName = (String,String)
 
 
-sample = BibRecord "kkkOi" Article [
+sample = BibRecord "kkkOi" [Article] [
   Title "Titulek" "Podtitulek", 
   Authors [
     Person "pepa" "novák",
     Person "Jirka" "Kájínek"
     ]
   ]
---ToDO: Pass test function
-fetchField :: (BibField -> Bool) -> [BibField] -> Maybe BibField
-fetchField _ [] = Nothing
-fetchField fn (x:xs) =  if (fn x) 
-                        then Just x
-                        else fetchField fn xs     
 
-testAuthor :: BibField -> Bool
-testAuthor (Authors _) = True
-testAuthor _ = False
+--better to use filter testFn [BibField]
+--example: filter (\s -> case s of (Title _ _) -> True; _ -> False) (bibFields sample)
+fetchField :: (BibField -> BibField ->  Bool) -> BibField  -> [BibField] -> Maybe BibField
+fetchField _ _ [] = Nothing
+fetchField fn t (x:xs) =  if (fn x t) 
+                          then Just x
+                          else fetchField fn t xs     
+
+fieldTest "title" = (\s -> case s of (Title _ _) -> True; _ -> False)
+fieldTest "author" = (\s -> case s of (Authors _ ) -> True; _ -> False)
+fieldTest _ = (\s -> False)
+
+testAuthor :: BibField -> BibField -> Bool
+testAuthor m n | m == n = True
+testAuthor _ _ = False          -- 
 --fetchField (_:xs) = fetchField xs
 
 showBibFields :: BibRecord -> String
